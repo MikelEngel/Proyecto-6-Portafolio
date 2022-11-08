e22e0226d8aa7ba980655536e7f714d108432a10
MikelEngel <angelhevel@gmail.com>
2022-11-08 13:18:41 -0600

    barra de menú

--- portafolio-personal/src/App.js
+++ portafolio-personal/src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { Button } from "@mui/material";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
 
 function App() {
   return (
-    <div>
-      <Button>
-        Soy un botón
-      </Button>
-    </div>
+    <div className="App">
+  
+      <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Miguel Ángel Hernández Velasco
+          </Typography>
+          <Button color="inherit">Sobre mi</Button>
+          <Button color="inherit">Proyectos</Button>
+          <Button color="inherit">Contáctame</Button>
+        </Toolbar>
+      </AppBar>
+
+
+    </div>  
   );
 }
 
