ce364a1668e860eb9c0bc0b89b9ad2e213ee49ff
MikelEngel <angelhevel@gmail.com>
2022-11-08 18:44:06 -0600

    about me

--- portafolio-personal/src/App.js
+++ portafolio-personal/src/App.js
@@ -1,26 +1,13 @@
 import React from "react";
-import { Button } from "@mui/material";
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import { NavBar, AboutMe } from "./components";
 
 
 function App() {
   return (
     <div className="App">
   
-      <AppBar position="static">
-        <Toolbar>
-          
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Miguel Ángel Hernández Velasco
-          </Typography>
-          <Button color="inherit">Sobre mi</Button>
-          <Button color="inherit">Proyectos</Button>
-          <Button color="inherit">Contáctame</Button>
-        </Toolbar>
-      </AppBar>
-
+    <NavBar />
+    <AboutMe />
 
     </div>  
   );
